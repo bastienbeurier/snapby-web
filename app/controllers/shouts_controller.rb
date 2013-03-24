15b7641cccac5115c0f348212c738c7297140415
Bastien Beurier <bastienbeurier@gmail.com>
2013-03-23 22:50:41 -0700

    Change create action

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -1,7 +1,7 @@
 class ShoutsController < ApplicationController
   def create
     Rails.logger.info "BAB params: #{params}"
-    shout = Shout.new(params[:shout])
+    shout = Shout.new(lat: params[:lat], lng: params[:lng], description: params[:description])
     success = shout.save
     Rails.logger.info "BAB shout: #{shout}"
     Rails.logger.info "BAB success? #{success}"
