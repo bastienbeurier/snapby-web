e7dd9a39e74e8cf27ed9f3269abe07563b71a3b4
Bastien Beurier <bastienbeurier@gmail.com>
2013-03-24 22:03:19 -0700

    Change route for pulling shouts

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -1,15 +1,13 @@
 class ShoutsController < ApplicationController
   def create
-    Rails.logger.info "BAB params: #{params}"
+    Rails.logger.info "BAB create params: #{params}"
     shout = Shout.new(lat: params[:lat], lng: params[:lng], description: params[:description])
     success = shout.save
-    Rails.logger.info "BAB shout: #{shout}"
-    Rails.logger.info "BAB success? #{success}"
     render :json => shout
   end
 
-  def index
-    Rails.logger.info "BAB #{params}"
+  def zone_shouts
+    Rails.logger.info "BAB index params: #{params}"
     shouts = Shout.within(:within => params[:radius], :origin => [params[:lat], params[:lng]])
     render :json => shouts
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 StreetShout::Application.routes.draw do
   resources :shouts
+  match "/zone_shouts" => "shouts#zone_shouts"
 end
