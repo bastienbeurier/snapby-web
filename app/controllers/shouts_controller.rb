72fbd47993fb7d3c6a36397862be209ea5729fa2
Bastien Beurier <bastienbeurier@gmail.com>
2013-03-24 22:13:11 -0700

    Change json response

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -3,13 +3,13 @@ class ShoutsController < ApplicationController
     Rails.logger.info "BAB create params: #{params}"
     shout = Shout.new(lat: params[:lat], lng: params[:lng], description: params[:description])
     success = shout.save
-    render :json => shout
+    shout
   end
 
   def zone_shouts
     Rails.logger.info "BAB index params: #{params}"
     shouts = Shout.within(:within => params[:radius], :origin => [params[:lat], params[:lng]])
-    render :json => shouts
+    shouts
   end
 end
   
