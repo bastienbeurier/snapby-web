d6f651aee07919e98c9d3ca7201c8f2c258b78cf
Bastien Beurier <bastienbeurier@gmail.com>
2013-03-24 22:28:46 -0700

    Never ends

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -3,15 +3,13 @@ class ShoutsController < ApplicationController
     Rails.logger.info "BAB create params: #{params}"
     shout = Shout.new(lat: params[:lat], lng: params[:lng], description: params[:description])
     success = shout.save
-    format.json { render :json => shout.to_json }
+    format.json { render :json => shout }
   end
 
   def zone_shouts
     Rails.logger.info "BAB index params: #{params}"
     shouts = Shout.within(:within => params[:radius], :origin => [params[:lat], params[:lng]])
-    Rails.logger.info "BAB shouts: #{shouts}"
-    Rails.logger.info "BAB length: #{shouts.length}"
-    format.json { render :json => shouts.to_json }
+    format.json { render :json => shouts }
   end
 end
   
