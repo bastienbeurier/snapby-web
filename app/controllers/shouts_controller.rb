19411e34fe62353486040a1dc2f710fd8a59664c
Bastien Beurier <bastienbeurier@gmail.com>
2013-03-25 09:38:59 -0700

    Comments and corrections

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -1,17 +1,23 @@
 class ShoutsController < ApplicationController
+
+  #Create a shout
   def create
     Rails.logger.info "BAB create params: #{params}"
+    
     shout = Shout.new(lat: params[:lat], lng: params[:lng], description: params[:description])
     success = shout.save
-    format.json { render :json => shout, status: :create }
+
     respond_to do |format|
       format.json { render json: {result: shout, status: 201} }
     end
   end
 
+  #Retrieve shouts within  a zone
   def zone_shouts
     Rails.logger.info "BAB index params: #{params}"
+
     shouts = Shout.within(:within => params[:radius], :origin => [params[:lat], params[:lng]])
+    
     respond_to do |format|
       format.json { render json: {result: shouts, status: 200} }
     end
