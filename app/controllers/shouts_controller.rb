f699a4c7de3ccfaed9437216dbb19fd7a35dc403
Bastien Beurier <bastienbeurier@gmail.com>
2013-03-24 22:35:09 -0700

    Add status to json response

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -3,13 +3,13 @@ class ShoutsController < ApplicationController
     Rails.logger.info "BAB create params: #{params}"
     shout = Shout.new(lat: params[:lat], lng: params[:lng], description: params[:description])
     success = shout.save
-    format.json { render :json => shout }
+    format.json { render :json => shout, status: :create }
   end
 
   def zone_shouts
     Rails.logger.info "BAB index params: #{params}"
     shouts = Shout.within(:within => params[:radius], :origin => [params[:lat], params[:lng]])
-    format.json { render :json => shouts }
+    format.json { render json: shouts, status: :ok }
   end
 end
   
