5b0fa88c4747c9690dfb0a5897bb58dec6a8a31b
Bastien Beurier <bastienbeurier@gmail.com>
2013-04-14 17:20:11 -0700

    Created_at accessible and better query

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -18,7 +18,7 @@ class ShoutsController < ApplicationController
     Rails.logger.info "BAB index params: #{params}"
     one_day_ago = Time.now - 1.day
 
-    shouts = Shout.within(params[:radius].to_i, :origin => [params[:lat], params[:lng]]).where("created_at >= :one_day_ago", {:one_day_ago => one_day_ago}).limit(100)
+    shouts = Shout.where("created_at >= :one_day_ago", {:one_day_ago => one_day_ago}).within(params[:radius].to_i, :origin => [params[:lat], params[:lng]]).limit(100)
 
     respond_to do |format|
       format.json { render json: {result: shouts, status: 200} }
--- app/models/shout.rb
+++ app/models/shout.rb
@@ -1,11 +1,11 @@
 class Shout < ActiveRecord::Base
-  acts_as_mappable  :default_units => :miles, 
+  acts_as_mappable  :default_units => :kms, 
                     :default_formula => :sphere, 
                     :distance_field_name => :distance,
                     :lat_column_name => :lat,
                     :lng_column_name => :lng
 
-  attr_accessible   :description, :lat, :lng, :source
+  attr_accessible   :description, :lat, :lng, :source, :created_at
 
   validates :description, presence: true, length: { maximum: 140 }
   validates :lat,         presence: true
