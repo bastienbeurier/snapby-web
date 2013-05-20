fb1bc48d6c8b79a94c500443809d4ddc79cb661c
Bastien Beurier <bastienbeurier@gmail.com>
2013-05-20 13:14:44 -0700

    Add query for global feed

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -15,7 +15,7 @@ class ShoutsController < ApplicationController
 
   #Retrieve shouts within  a zone
   def zone_shouts
-    Rails.logger.info "BAB index params: #{params}"
+    Rails.logger.info "BAB zone_shouts params: #{params}"
     one_day_ago = Time.now - 1.day
 
     if params[:notwitter].to_i == 1
@@ -23,6 +23,21 @@ class ShoutsController < ApplicationController
     else 
       shouts = Shout.where("created_at >= :one_day_ago", {:one_day_ago => one_day_ago}).within(params[:radius].to_i, :origin => [params[:lat], params[:lng]]).limit(100)
     end
+
+    respond_to do |format|
+      format.json { render json: {result: shouts, status: 200} }
+      format.html { render json: shouts }
+    end
+  end
+
+  #Retrive most recent shouts for the feed with pagination
+  def global_feed_shouts
+    Rails.logger.info "BAB global_feed_shouts params: #{params}"
+
+    per_page = params[:per_page] ? params[:per_page] : 20
+    page = params[:page] ? params[:page] : 1
+
+    shouts = Shout.where("source = 'native'").paginage(page: page, per_page: per_page).order('id DESC')
 
     respond_to do |format|
       format.json { render json: {result: shouts, status: 200} }
