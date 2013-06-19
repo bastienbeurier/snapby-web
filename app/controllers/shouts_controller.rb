3db53927e698344e9cea2042ed4ca6a042e5ce7e
Bastien Beurier <bastienbeurier@gmail.com>
2013-06-19 16:38:12 -0700

    Debug 7

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -22,33 +22,33 @@ class ShoutsController < ApplicationController
 
   #Retrieve shouts within a zone (location and radius)
   def zone_shouts
-    # Rails.logger.info "BAB zone_shouts params: #{params}"
-    # max_age = Time.now - 24.hours
+    Rails.logger.info "BAB zone_shouts params: #{params}"
+    max_age = Time.now - 24.hours
 
-    # if params[:notwitter].to_i == 1
-    #   shouts = Shout.where("source = 'native' AND created_at >= :max_age", {:max_age => max_age}).within(params[:radius].to_i, :origin => [params[:lat], params[:lng]]).limit(100).order("created_at DESC")
-    # else 
-    #   shouts = Shout.where("created_at >= :max_age", {:max_age => max_age}).within(params[:radius].to_i, :origin => [params[:lat], params[:lng]]).limit(100).order("created_at DESC")
-    # end
+    if params[:notwitter].to_i == 1
+      shouts = Shout.where("source = 'native' AND created_at >= :max_age", {:max_age => max_age}).within(params[:radius].to_i, :origin => [params[:lat], params[:lng]]).limit(100).order("created_at DESC")
+    else 
+      shouts = Shout.where("created_at >= :max_age", {:max_age => max_age}).within(params[:radius].to_i, :origin => [params[:lat], params[:lng]]).limit(100).order("created_at DESC")
+    end
 
-    # respond_to do |format|
-    #   format.json { render json: {result: shouts, status: 200} }
-    #   format.html { render json: shouts }
-    # end
+    respond_to do |format|
+      format.json { render json: {result: shouts, status: 200} }
+      format.html { render json: shouts }
+    end
   end
 
-  # #Retrieve shouts within a zone (bouding box)
-  # def bound_box_shouts
-  #   # Rails.logger.info "BAB zone_shouts params: #{params}"
-  #   # max_age = Time.now - 24.hours
+  #Retrieve shouts within a zone (bouding box)
+  def bound_box_shouts
+    Rails.logger.info "BAB zone_shouts params: #{params}"
+    max_age = Time.now - 24.hours
 
-  #   # shouts = Shout.where("created_at >= :max_age", {:max_age => max_age}).in_bounds([[params[:neLat], params[:neLng]], [params[:swLat], params[:swLng]]]).limit(100).order("created_at DESC")
+    shouts = Shout.where("created_at >= :max_age", {:max_age => max_age}).in_bounds([[params[:neLat], params[:neLng]], [params[:swLat], params[:swLng]]]).limit(100).order("created_at DESC")
 
-  #   # respond_to do |format|
-  #   #   format.json { render json: {result: shouts, status: 200} }
-  #   #   format.html { render json: shouts }
-  #   # end
-  # end
+    respond_to do |format|
+      format.json { render json: {result: shouts, status: 200} }
+      format.html { render json: shouts }
+    end
+  end
 
   #Not used anymore
   #Retrieve most recent shouts for the feed with pagination
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 StreetShout::Application.routes.draw do
   resources :shouts
   match "/zone_shouts" => "shouts#zone_shouts"
-  # match "/bound_box_shouts" => "shouts#bound_box_shouts"
+  match "/bound_box_shouts" => "shouts#bound_box_shouts"
   match "/global_feed_shouts" => "shouts#global_feed_shouts"
 end
 	
