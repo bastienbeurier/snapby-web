c81c6e701c304afb5ec8d7aa66e10b520a405ee1
Bastien Beurier <bastienbeurier@gmail.com>
2013-06-20 13:51:56 -0700

    Urban Airship config

---
+++ config/initializers/urban_airship.rb
@@ -0,0 +1,5 @@
+Urbanairship.application_key = 'AzqCIDWGSqmEOJvM95Vfqw'
+Urbanairship.application_secret = '_YOtj7i6RyeelX_BE9AohQ'
+Urbanairship.master_secret = 'Jv4zxUycQC2ASS--Ea-1Fg'
+Urbanairship.logger = Rails.logger
+Urbanairship.request_timeout = 5
