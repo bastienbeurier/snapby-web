c4f1139d2f097bfa2cbca608fc4ec3253522f75a
Bastien Beurier <bastienbeurier@gmail.com>
2013-06-21 12:46:57 -0700

    Debug 4

--- app/observers/shout_observer.rb
+++ app/observers/shout_observer.rb
@@ -2,7 +2,7 @@ class ShoutObserver < ActiveRecord::Observer
   # include PushNotification
 
   def after_create(shout)
-    devices = Device.within(10, :origin => [shout.lat, shout.lng]).where("notification_radius != 0")
+    devices = Device.within(10, :origin => [shout.lat, shout.lng]).where("notification_radius IS NULL OR notification_radius > 0")
 
 	push_tokens = devices.map { |device| device.push_token }
 
