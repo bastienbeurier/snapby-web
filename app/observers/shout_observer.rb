16436adde4c531dee111980adecb5abd270c09eb
Bastien Beurier <bastienbeurier@gmail.com>
2013-06-21 14:23:25 -0700

    For now notify all users for all shouts (exept theirs)

--- app/observers/shout_observer.rb
+++ app/observers/shout_observer.rb
@@ -3,7 +3,8 @@ class ShoutObserver < ActiveRecord::Observer
   # include PushNotification
 
   def after_create(shout)
-    devices = Device.within(10, :origin => [shout.lat, shout.lng]).where("notification_radius != 0 AND device_id != :shout_device_id", {shout_device_id: shout.device_id})
+    # devices = Device.within(10, :origin => [shout.lat, shout.lng]).where("notification_radius != 0 AND device_id != :shout_device_id", {shout_device_id: shout.device_id})
+    devices = Device.where("notification_radius != 0 AND device_id != :shout_device_id", {shout_device_id: shout.device_id})
 
 	push_tokens = devices.map { |device| device.push_token }
 
