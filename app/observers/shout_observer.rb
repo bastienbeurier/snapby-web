5784e5ee2a4fd64e9f67951b4db42915ddc3fe21
Bastien Beurier <bastienbeurier@gmail.com>
2013-06-21 12:14:09 -0700

    Debug 3

--- app/observers/shout_observer.rb
+++ app/observers/shout_observer.rb
@@ -2,6 +2,13 @@ class ShoutObserver < ActiveRecord::Observer
   # include PushNotification
 
   def after_create(shout)
-    # PushNotification.notify_new_shout(shout)
+    devices = Device.within(10, :origin => [shout.lat, shout.lng]).where("notification_radius != 0")
+
+	push_tokens = devices.map { |device| device.push_token }
+
+	begin
+	  	Urbanairship.push({apids: push_tokens, android: {alert: shout.description}})
+	rescue Exception => e
+	end
   end
 end
