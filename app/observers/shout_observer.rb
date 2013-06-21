9771d67cc321f4cc572f6ded74b32fd3a9fd0e09
Bastien Beurier <bastienbeurier@gmail.com>
2013-06-21 12:06:39 -0700

    Renaming

--- app/observers/shout_observer.rb
+++ app/observers/shout_observer.rb
@@ -2,6 +2,6 @@ class ShoutObserver < ActiveRecord::Observer
   include PushNotification
 
   def after_create(shout)
-    PushNotification.new_shout(shout)
+    PushNotification.notify_new_shout(shout)
   end
 end
--- lib/push_notification.rb
+++ lib/push_notification.rb
@@ -1,5 +1,5 @@
 module PushNotification
-	def self.new_shout(shout)
+	def self.notify_new_shout(shout)
 		#Add condition where device id != shout device id (no notification to sender)
 		#Default radius to 10km for now
 		devices = Device.within(10, :origin => [shout.lat, shout.lng]).where("notification_radius != 0")
