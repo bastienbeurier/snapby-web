bc973fc4e26c658f417a44c800da78996c4973b5
Bastien Beurier <bastienbeurier@gmail.com>
2013-06-20 17:55:05 -0700

    Save device info in DB

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,4 +1,43 @@
 class DevicesController < ApplicationController
-  def new
+  def update_device_info
+    Rails.logger.info "BAB update_device_info: #{params}"
+
+    device = Device.find_by_device_id(params[:device_id])
+
+    if device 
+      device.push_token = params[:push_token]
+      device.os_version = params[:os_version]
+      device.app_version = params[:app_version]
+      device.api_version = params[:api_version]
+      if params[:lat] and params[:lng]
+        device.lat = params[:lat]
+        device.lng = params[:lng]
+      end
+    else
+      device = Device.new(device_id: params[:device_id], 
+                          push_token: params[:push_token], 
+                          device_model: params[:device_model], 
+                          os_version: params[:os_version], 
+                          os_type: params[:os_type], 
+                          app_version: params[:app_version], 
+                          api_version: params[:api_version], 
+                          lat: params[:lat], 
+                          lng: params[:lng])
+    end
+
+    
+    success = device.save
+
+    if success
+      respond_to do |format|
+        format.json { render json: {result: device, status: 201} }
+        format.html { render json: device }
+      end
+    else 
+      respond_to do |format|
+        format.json { render(:json => { :errors => "Failed to save device", :errorStatusCode => "Failed to save shout" }, :status => 500) }
+        format.html { render(:text => "Failed to save device", :status => 500) }
+      end 
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 StreetShout::Application.routes.draw do
-  get "devices/new"
-
   resources :shouts
   match "/zone_shouts" => "shouts#zone_shouts"
   match "/bound_box_shouts" => "shouts#bound_box_shouts"
   match "/global_feed_shouts" => "shouts#global_feed_shouts"
+
+  match "/update_device_info" => "devices#update_device_info"
 end
 	
