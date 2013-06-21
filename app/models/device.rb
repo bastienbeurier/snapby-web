b0e60a86545688220d74013f10aca8f6cffdd28e
Bastien Beurier <bastienbeurier@gmail.com>
2013-06-20 18:42:59 -0700

    Validates uniqueness of device id

--- app/models/device.rb
+++ app/models/device.rb
@@ -6,4 +6,6 @@ class Device < ActiveRecord::Base
                     :lng_column_name => :lng
 
   attr_accessible :api_version, :app_version, :device_id, :device_model, :lat, :lng, :notification_radius, :os_type, :os_version, :push_token
+
+  validates_uniqueness_of :device_id
 end
