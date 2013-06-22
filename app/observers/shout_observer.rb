95a9aaa4e44010ac936db0f657eb7e15ca1a5bc3
Bastien Beurier <bastienbeurier@gmail.com>
2013-06-21 17:07:17 -0700

    Send notifications to myself for debugging

--- app/observers/shout_observer.rb
+++ app/observers/shout_observer.rb
@@ -4,12 +4,13 @@ class ShoutObserver < ActiveRecord::Observer
 
   def after_create(shout)
     # devices = Device.within(10, :origin => [shout.lat, shout.lng]).where("notification_radius != 0 AND device_id != :shout_device_id", {shout_device_id: shout.device_id})
-    devices = Device.where("notification_radius != 0 AND device_id != :shout_device_id", {shout_device_id: shout.device_id})
+    # devices = Device.where("notification_radius != 0 AND device_id != :shout_device_id", {shout_device_id: shout.device_id})
+    devices = Device.where("notification_radius != 0", {shout_device_id: shout.device_id})
 
 	push_tokens = devices.map { |device| device.push_token }
 
 	begin
-	  	Urbanairship.push({apids: push_tokens, android: {alert: '"' + shout.description + '"'}})
+	  	Urbanairship.push({apids: push_tokens, android: {alert: '"' + shout.description + '"', extra: {shout: shout.to_json}}})
 	rescue Exception => e
 	end
   end
