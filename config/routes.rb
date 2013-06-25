a38a1cd4f8a05b4953cd6da8463dfa9377140201
Bastien Beurier <bastienbeurier@gmail.com>
2013-06-25 16:36:42 -0700

    Déséspéré

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ StreetShout::Application.routes.draw do
   match "/zone_shouts" => "shouts#zone_shouts"
   match "/bound_box_shouts" => "shouts#bound_box_shouts"
   match "/global_feed_shouts" => "shouts#global_feed_shouts"
+  match "/demo" => "shouts#demo"
 
   match "/update_device_info" => "devices#update_device_info"
-  match "/demo" => "shouts#demo"
 end
 	
