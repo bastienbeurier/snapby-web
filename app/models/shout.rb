3b26d1ad3c1d121f917e9d18fd9942841d52b30a
Bastien Beurier <bastienbeurier@gmail.com>
2013-07-01 14:27:57 -0700

    add image as attr_accessible

--- app/models/shout.rb
+++ app/models/shout.rb
@@ -5,7 +5,7 @@ class Shout < ActiveRecord::Base
                     :lat_column_name => :lat,
                     :lng_column_name => :lng
 
-  attr_accessible   :description, :lat, :lng, :source, :created_at, :display_name, :device_id
+  attr_accessible   :description, :lat, :lng, :source, :created_at, :display_name, :device_id, :image
 
   validates :description, presence: true, length: { maximum: 140 }
   validates :lat,         presence: true
