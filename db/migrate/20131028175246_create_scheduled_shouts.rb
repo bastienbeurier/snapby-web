f431704a803c42009f5846ae0c757c81f2c7d897
Bastien Beurier <bastienbeurier@gmail.com>
2013-10-28 18:56:47 +0100

    Scheduled shouts table create migration

---
+++ db/migrate/20131028175246_create_scheduled_shouts.rb
@@ -0,0 +1,15 @@
+class CreateScheduledShouts < ActiveRecord::Migration
+  def change
+    create_table :shouts do |t|
+      t.float :lat
+      t.float :lng
+      t.datetime :scheduled_time
+      t.string :description
+      t.string :display_name
+      t.string :image
+      t.string :author
+
+      t.timestamps
+    end
+  end
+end
