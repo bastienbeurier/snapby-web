67b63145884213c359a34602d69556293415f69b
Bastien Beurier <bastienbeurier@gmail.com>
2013-11-13 15:47:22 +0100

    shout_created_at field in RemovedShouts model

--- app/models/removed_shout.rb
+++ app/models/removed_shout.rb
@@ -1,11 +1,12 @@
 class RemovedShout < ActiveRecord::Base
-  attr_accessible   :shout_id, :removed_by, :description, :lat, :lng, :source, :created_at, :display_name, :device_id, :image
+  attr_accessible   :shout_id, :removed_by, :shout_created_at, :description, :lat, :lng, :source, :created_at, :display_name, :device_id, :image
 
-  validates :description, presence: true, length: { maximum: 140 }
-  validates :lat,         presence: true
-  validates :lng,         presence: true
-  validates :source,      presence: true
-  validates :display_name,presence: true
-  validates :shout_id,    presence: true
-  validates :removed_by,  presence: true
+  validates :description,      presence: true, length: { maximum: 140 }
+  validates :lat,              presence: true
+  validates :lng,              presence: true
+  validates :source,           presence: true
+  validates :display_name,     presence: true
+  validates :shout_id,         presence: true
+  validates :removed_by,  	   presence: true
+  validates :shout_created_at, presence: true
 end
