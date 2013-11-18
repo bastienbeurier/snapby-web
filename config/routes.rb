1718d738f417a94142a7bc7195048cf7a8c9f2b8
Bastien Beurier <bastienbeurier@gmail.com>
2013-11-18 16:29:20 +0100

    Add home controller and website index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 StreetShout::Application.routes.draw do
   resources :shouts
 
+  root :to => "home#index"
+
   match "/zone_shouts" => "shouts#zone_shouts"
   match "/bound_box_shouts" => "shouts#bound_box_shouts"
   match "/global_feed_shouts" => "shouts#global_feed_shouts"
