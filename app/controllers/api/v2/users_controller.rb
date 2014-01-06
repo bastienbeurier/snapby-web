f2cb29ad7aa6a5d56a56b7fcbc3fa0151a324eab
Bastien Beurier <bastienbeurier@gmail.com>
2014-01-06 16:00:42 +0100

    Add create user action in user controller

--- app/controllers/api/v2/users_controller.rb
+++ app/controllers/api/v2/users_controller.rb
@@ -1,3 +1,19 @@
 class Api::V2::UsersController < Api::V2::ApiController
     skip_before_filter :authenticate_user!, :only => [:create]
+
+    def create
+        user = User.new(user_params)
+
+        if user.save
+            render json: { result: user }, status: 201
+        else
+            render json: { :errors => user.errors }, status: 422
+        end
+    end
+
+    private 
+
+        def user_params
+            params.permit(:email, :password, :password_confirmation)
+        end
 end
