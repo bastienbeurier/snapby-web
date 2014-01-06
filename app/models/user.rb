89814bdf205496d63c02e360d7d096565b6a933e
Bastien Beurier <bastienbeurier@gmail.com>
2014-01-06 17:06:10 +0100

    Remove devise trackable attribute, remove unused users columns

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :token_authenticatable
+         :recoverable, :rememberable, :validatable, :token_authenticatable
 end
