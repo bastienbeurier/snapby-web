f3818e5ca264499eb825996960f3c4f15f156dfb
Baptiste <baptiste.truchot@gmail.com>
2014-01-08 12:40:10 +0100

    Change logic flag shout

--- app/controllers/api/v2/flags_controller.rb
+++ app/controllers/api/v2/flags_controller.rb
@@ -15,22 +15,23 @@ class Api::V2::FlagsController < Api::V2::ApiController
 
     #Case where this user already flagged this shout
     if existing_flags.find_by(flagger_id: flag.flagger_id)
-      format.json { render json: {errors: ["Shout already flagged by user"]}, status: 422
+      render json: {errors: ["Shout already flagged by user"]}, status: 422
       return
     else
       flag = Flag.new(flag_params)
-      #if more than 5 flags, do not display the shout anymore
-      if existing_flags.count >= 4
-        shout.update_attributes(removed: true)
-      end
-
-      #send mail (specified if automatically removed)
-      UserMailer.flagged_shout_email(flag,shout).deliver
 
       if flag.save
-        format.json { render json: {message: "Shout successfully flagged"}, status: 200
+        #if more than 5 flags, do not display the shout anymore
+        if existing_flags.count >= 5
+          shout.update_attributes(removed: true)
+        end
+
+        #send mail (specified if automatically removed)
+        UserMailer.flagged_shout_email(flag,shout).deliver
+
+        render json: {message: "Shout successfully flagged"}, status: 200
       else 
-        format.json { render json: {errors: ["Failed to flag shout"]}, status: 500
+        render json: {errors: ["Failed to flag shout"]}, status: 500 
       end
     end
   end
