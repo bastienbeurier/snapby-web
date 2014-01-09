82481c1532d23abe234bbab1c76bdd03491108aa
Bastien Beurier <bastienbeurier@gmail.com>
2014-01-09 16:27:11 +0100

    Remove uniqueness of email in user model cause devise takes care of it

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :token_authenticatable, :validatable
 
   #devise automatically checks email format and convert to lower case
-  validates_uniqueness_of :email, :username
+  #devise validates uniqueness of email
+  validates_uniqueness_of :username
   validates :email, presence: true
   validates :password, presence: true #between 6..128 chars (defined in Devise config)
   validates :username, presence: true, length: { minimum: 6, maximum: 20 }
