5ee04cde16e25ce2cfa08965dd0911095ce2b358
Bastien Beurier <bastienbeurier@gmail.com>
2014-01-09 16:16:04 +0100

    Add length minimum condition for username

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,6 @@ class User < ActiveRecord::Base
   #devise automatically checks email format and convert to lower case
   validates_uniqueness_of :email
   validates :email, presence: true
-  validates :password, presence: true
-  validates :username, presence: true, length: { maximum: 20 }
+  validates :password, presence: true #between 6..128 chars (defined in Devise config)
+  validates :username, presence: true, length: { minimum: 6, maximum: 20 }
 end
