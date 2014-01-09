15e7e4512c1c9038bfcae80ef016e0fed52383be
Bastien Beurier <bastienbeurier@gmail.com>
2014-01-09 16:24:58 +0100

    Validates uniqueness of username

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :token_authenticatable, :validatable
 
   #devise automatically checks email format and convert to lower case
-  validates_uniqueness_of :email
+  validates_uniqueness_of :email, :username
   validates :email, presence: true
   validates :password, presence: true #between 6..128 chars (defined in Devise config)
   validates :username, presence: true, length: { minimum: 6, maximum: 20 }
