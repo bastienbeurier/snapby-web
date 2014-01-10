8ba56b872080eb240690215d8b215040853f578e
Baptiste <baptiste.truchot@gmail.com>
2014-01-10 18:43:59 +0100

    Change path generated by email 5

--- app/controllers/api/v2/passwords_controller.rb
+++ app/controllers/api/v2/passwords_controller.rb
@@ -9,8 +9,8 @@ class Api::V2::PasswordsController < Devise::PasswordsController
 
   def create
     user = User.find_by(email: params[:email])
-    @path = edit_api_v2_user_password_url(user, :reset_password_token => user.reset_password_token) 
-    user.send_reset_password_instructions
+    
+    user.send_reset_password_instructions # Careful: the path in the mailer is API dependent 
 
     render json: { result: { messages: ["Reset password instructions have been sent to #{user.email}."] } }, status: 201
   end
