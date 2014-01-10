4607530e0ba4c7084ace61cf2990f78489147e10
Baptiste <baptiste.truchot@gmail.com>
2014-01-10 20:08:35 +0100

    Try to fix password resetting 2

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -13,10 +13,12 @@ class PasswordsController < Devise::PasswordsController
 	end
 
 	def update
+    Rails.logger.debug "TRUCHOV update 1"
 		self.resource = resource_class.reset_password_by_token(resource_params)
+    Rails.logger.debug "TRUCHOV update 1"
     yield resource if block_given?
 
-    Rails.logger.debug "BAB update user: #{resource.errors}"
+    Rails.logger.debug "TRUCHOV update user: #{resource.errors}"
     if resource.errors.empty?
       resource.unlock_access! if unlockable?(resource)
       flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
