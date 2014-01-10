de84037a463ef986e2f8303cc77f280e21bf3562
Baptiste <baptiste.truchot@gmail.com>
2014-01-10 16:37:23 +0100

    override create password

--- app/controllers/api/v2/passwords_controller.rb
+++ app/controllers/api/v2/passwords_controller.rb
@@ -3,9 +3,8 @@ class Api::V2::PasswordsController < Devise::PasswordsController
   # Auth?
 
   def create
-    super
-    # user = User.find_by(email: params[:email])
-    # user.send_reset_password_instructions
+    user = User.find_by(email: params[:email])
+    user.send_reset_password_instructions
     render json: { result: { messages: ["Reset password instructions have been sent to #{user.email}."] } }, status: 201
   end
 
