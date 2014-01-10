fe2d3d2c559b1708aea29de620e05f82a1b31066
Baptiste <baptiste.truchot@gmail.com>
2014-01-10 18:41:11 +0100

    Bastien - validation change

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
 
   #devise automatically checks email format and convert to lower case
   #devise validates uniqueness of email
-  validates_uniqueness_of :username
+  validates_uniqueness_of :username, case_sensitive: false
   validates :email, presence: true
   validates :password, presence: true , on: :create #between 6..128 chars (defined in Devise config)
   validates :username, presence: true, length: { minimum: 6, maximum: 20 }
