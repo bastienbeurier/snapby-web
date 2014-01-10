bea2c24d070f588a99783193dfc7bed7488c1630
Baptiste <baptiste.truchot@gmail.com>
2014-01-10 15:34:46 +0100

    Test devise route

--- app/controllers/api/v2/passwords_controller.rb
+++ app/controllers/api/v2/passwords_controller.rb
@@ -1,10 +1,18 @@
 class Api::V2::PasswordsController < Api::V2::ApiController
   skip_before_filter :authenticate_user!, :only => :create
 
+  def new
+    
+  end
+
   def create
     user = User.find_by(email: params[:email])
     user.send_reset_password_instructions
     render json: { result: { messages: ["Reset password instructions have been sent to #{user.email}."] } }, status: 201
+  end
+
+  def update
+
   end
 
 end
