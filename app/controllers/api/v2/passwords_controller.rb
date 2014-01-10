271fa39ac1915bce513be0032422d16233e4a5c9
Baptiste <baptiste.truchot@gmail.com>
2014-01-10 16:11:20 +0100

    Password controller now inherits from devise

--- app/controllers/api/v2/passwords_controller.rb
+++ app/controllers/api/v2/passwords_controller.rb
@@ -1,15 +1,20 @@
-class Api::V2::PasswordsController < Api::V2::ApiController
-  skip_before_filter :authenticate_user!, :only => :create
-
+class Api::V2::PasswordsController < Devise::PasswordsController
+  
+  # Auth?
 
   def create
-    user = User.find_by(email: params[:email])
-    user.send_reset_password_instructions
-    render json: { result: { messages: ["Reset password instructions have been sent to #{user.email}."] } }, status: 201
+    super
+    # user = User.find_by(email: params[:email])
+    # user.send_reset_password_instructions
+    # render json: { result: { messages: ["Reset password instructions have been sent to #{user.email}."] } }, status: 201
+  end
+
+  def edit
+    super
   end
 
   def update
-
+    super
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 StreetShout::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :skip => [:registrations,:passwords] 
   resources :shouts
 
   root :to => "home#index"
@@ -26,7 +26,7 @@ StreetShout::Application.routes.draw do
       resources :users, only: [:create, :update]
       resources :flags, only: [:create]
       resources :shouts, only: [:create, :show]
-      devise_for :users, :controllers => { sessions:'api/v2/sessions', passwords:'api/v2/passwords' } # custom controller for API token access with devise
+      devise_for :users, :skip => [:registrations], :controllers => { sessions:'api/v2/sessions', passwords:'api/v2/passwords' } # custom controller for API token access with devise
 
       get "/obsolete_api" => "api#obsolete_api"
       get "/bound_box_shouts" => "shouts#bound_box_shouts"
