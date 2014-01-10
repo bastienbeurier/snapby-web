66d2a99543c772c68a9c38b30a4810555f63289e
Baptiste <baptiste.truchot@gmail.com>
2014-01-10 17:25:05 +0100

    Test if update of devise is working

--- app/controllers/api/v2/passwords_controller.rb
+++ app/controllers/api/v2/passwords_controller.rb
@@ -8,9 +8,10 @@ class Api::V2::PasswordsController < Devise::PasswordsController
   end
 
   def create
-    user = User.find_by(email: params[:email])
-    user.send_reset_password_instructions
-    render json: { result: { messages: ["Reset password instructions have been sent to #{user.email}."] } }, status: 201
+    super
+    # user = User.find_by(email: params[:email])
+    # user.send_reset_password_instructions
+    # render json: { result: { messages: ["Reset password instructions have been sent to #{user.email}."] } }, status: 201
   end
 
   def edit
