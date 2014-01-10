63465c2798cdc994c92ea071bf7dfaea0133dd51
Baptiste <baptiste.truchot@gmail.com>
2014-01-10 21:05:24 +0100

    test

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -14,20 +14,24 @@ class PasswordsController < Devise::PasswordsController
 
 	def update
     Rails.logger.debug "TRUCHOV update 1"
-		self.resource = resource_class.reset_password_by_token(resource_params)
-    Rails.logger.debug "TRUCHOV update 2"
-    yield resource if block_given?
+		# self.resource = resource_class.reset_password_by_token(resource_params)
+  #   Rails.logger.debug "TRUCHOV update 2"
+  #   yield resource if block_given?
 
-    Rails.logger.debug "TRUCHOV update user: #{resource.errors}"
-    if resource.errors.empty?
-      resource.unlock_access! if unlockable?(resource)
-      flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
-      set_flash_message(:notice, flash_message) if is_flashing_format?
-      sign_in(resource_name, resource)
-      respond_with resource, :location => after_resetting_password_path_for(resource)
-    else
-      respond_with resource
-    end
+  #   Rails.logger.debug "TRUCHOV update user: #{resource.errors}"
+  #   if resource.errors.empty?
+  #     resource.unlock_access! if unlockable?(resource)
+  #     flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
+  #     set_flash_message(:notice, flash_message) if is_flashing_format?
+  #     sign_in(resource_name, resource)
+  #     respond_with resource, :location => after_resetting_password_path_for(resource)
+  #   else
+  #     respond_with resource
+  #   end
+    user = User.find_by(reset_password_token: params[:reset_password_token])
+    user.password = params[:password]
+    user.password_confirmation = params[:password_confirmation]
+    Rails.logger.debug "TRUCHOV update 1 #{user.save}"
 	end
 
 end
