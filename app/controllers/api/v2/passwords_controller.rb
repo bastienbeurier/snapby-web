e892f007203d99c96105210f3b5b1cf7b7491b2d
Baptiste <baptiste.truchot@gmail.com>
2014-01-10 17:23:13 +0100

    Je suis perdu

--- app/controllers/api/v2/passwords_controller.rb
+++ app/controllers/api/v2/passwords_controller.rb
@@ -2,21 +2,23 @@ class Api::V2::PasswordsController < Devise::PasswordsController
   
   # Auth?
 
+  #delete route
   def new
+    render nothing:true
   end
 
   def create
-    # user = User.find_by(email: params[:email])
-    # user.send_reset_password_instructions
-    # render json: { result: { messages: ["Reset password instructions have been sent to #{user.email}."] } }, status: 201
+    user = User.find_by(email: params[:email])
+    user.send_reset_password_instructions
+    render json: { result: { messages: ["Reset password instructions have been sent to #{user.email}."] } }, status: 201
   end
 
   def edit
-  #   super
+     super
   end
 
   def update
-  #   super
+    super
   end
 
 end
