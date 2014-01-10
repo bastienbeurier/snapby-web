ae8b3fa57418609506e188067a8deb7ae16b6cca
Baptiste <baptiste.truchot@gmail.com>
2014-01-10 16:40:13 +0100

    override create password 2

--- app/controllers/api/v2/passwords_controller.rb
+++ app/controllers/api/v2/passwords_controller.rb
@@ -8,12 +8,12 @@ class Api::V2::PasswordsController < Devise::PasswordsController
     render json: { result: { messages: ["Reset password instructions have been sent to #{user.email}."] } }, status: 201
   end
 
-  def edit
-    super
-  end
+  # def edit
+  #   super
+  # end
 
-  def update
-    super
-  end
+  # def update
+  #   super
+  # end
 
 end
