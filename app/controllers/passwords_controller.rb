e9860adb052102e541dabc85c76844eb4c850d08
Baptiste <baptiste.truchot@gmail.com>
2014-01-10 21:29:42 +0100

    test

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -9,29 +9,26 @@ class PasswordsController < Devise::PasswordsController
 	end
 
 	def edit
-		render nothing: true
+		super
 	end
 
 	def update
     Rails.logger.debug "TRUCHOV update 1"
-		# self.resource = resource_class.reset_password_by_token(resource_params)
-  #   Rails.logger.debug "TRUCHOV update 2"
-  #   yield resource if block_given?
+		self.resource = resource_class.reset_password_by_token(resource_params)
+    Rails.logger.debug "TRUCHOV update 2"
+    yield resource if block_given?
 
-  #   Rails.logger.debug "TRUCHOV update user: #{resource.errors}"
-  #   if resource.errors.empty?
-  #     resource.unlock_access! if unlockable?(resource)
-  #     flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
-  #     set_flash_message(:notice, flash_message) if is_flashing_format?
-  #     sign_in(resource_name, resource)
-  #     respond_with resource, :location => after_resetting_password_path_for(resource)
-  #   else
-  #     respond_with resource
-  #   end
-    user = User.find_by(reset_password_token: params[:reset_password_token])
-    user.password = params[:password]
-    user.password_confirmation = params[:password_confirmation]
-    Rails.logger.debug "TRUCHOV update 1 #{user.save}"
+    Rails.logger.debug "TRUCHOV update user: #{resource.errors.to_s}"
+    if resource.errors.empty?
+      resource.unlock_access! if unlockable?(resource)
+      flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
+      set_flash_message(:notice, flash_message) if is_flashing_format?
+      sign_in(resource_name, resource)
+      respond_with resource, :location => after_resetting_password_path_for(resource)
+    else
+      respond_with resource
+    end
+
 	end
 
 end
