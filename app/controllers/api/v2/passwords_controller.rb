0162a5d5744285ffe3b75a8642c04453aba4f2c5
Baptiste <baptiste.truchot@gmail.com>
2014-01-10 16:30:31 +0100

    Try to fix issues by removing non used views 2

--- app/controllers/api/v2/passwords_controller.rb
+++ app/controllers/api/v2/passwords_controller.rb
@@ -6,7 +6,7 @@ class Api::V2::PasswordsController < Devise::PasswordsController
     super
     # user = User.find_by(email: params[:email])
     # user.send_reset_password_instructions
-    # render json: { result: { messages: ["Reset password instructions have been sent to #{user.email}."] } }, status: 201
+    render json: { result: { messages: ["Reset password instructions have been sent to #{user.email}."] } }, status: 201
   end
 
   def edit
