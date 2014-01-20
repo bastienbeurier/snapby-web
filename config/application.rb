6e8b232a42f0f15ca5bfffdfd68533e20b2d8d9a
Baptiste <baptiste.truchot@gmail.com>
2014-01-20 12:30:47 +0100

    New attempt

--- config/application.rb
+++ config/application.rb
@@ -49,7 +49,7 @@ module StreetShout
     # Enable the asset pipeline
     config.assets.enabled = true
     config.assets.paths << "#{Rails.root}/app/assets/fonts"
-    config.assets.precompile = ['*.js', '*.css']
+    config.assets.precompile += ['*.js', '*.css']
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
