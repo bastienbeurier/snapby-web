5b976ac5b383a07243be57d949c3313765a2f4b0
Bastien Beurier <bastienbeurier@gmail.com>
2014-01-22 16:37:09 +0100

    Implement likes

---
+++ app/controllers/api/v2/likes_controller.rb
@@ -0,0 +1,40 @@
+class Api::V2::LikesController < Api::V2::ApiController
+
+  #User creates like
+  def create
+    Rails.logger.debug "BAB create like params: #{params}"
+
+    params[:liker_id] = current_user.id
+    params[:liker_username] = current_user.username
+
+    like = Like.new(like_params)
+
+    if like.save
+      shout = Shout.find(params[:shout_id])
+      render json: { result: { like_count: shout.likes.count } }, status: 201
+    else 
+      render json: { errors: { internal: like.errors } }, :status => 500
+    end
+  end
+
+  #Display likes for a shout
+  def index
+    Rails.logger.debug "BAB index likes params: #{params}"
+
+    if !params[:shout_id]
+      render json: { errors: {incomplete: ["Incomplete like information"] } }, :status => 406
+      return
+    end
+
+    shout = Shout.find(params[:shout_id])
+
+    #TODO: handle when there is a lot of likes (not for now)
+    render json: {result: {likes: shout.likes.reverse } }, status: 200
+  end
+
+private
+
+  def like_params
+    params.permit(:shout_id, :liker_id, :liker_username, :lat, :lng)
+  end 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,7 @@ StreetShout::Application.routes.draw do
       resources :flags, only: [:create]
       resources :shouts, only: [:create, :show]
       resources :comments, only: [:create, :index]
+      resources :likes, only: [:create, :index]
       devise_for :users, :skip => [:registrations], :controllers => { sessions:'api/v2/sessions', passwords:'api/v2/passwords' } # custom controller for API token access with devise
 
       get "/obsolete_api" => "api#obsolete_api"
