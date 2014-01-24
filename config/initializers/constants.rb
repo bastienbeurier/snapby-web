387c77905ff9485c9504764f4e2f4df007935a5d
Baptiste <baptiste.truchot@gmail.com>
2014-01-24 15:31:53 +0100

    80

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -8,10 +8,10 @@ MAX_USERNAME_LENGTH = 20
 MIN_USERNAME_LENGTH = 6
 
 SHOUT_COMPANY_NAME = "Street Shout Inc."
-SHOUT_TRADEMARK_NAME = "Shout, Here & Now\u2122"
+SHOUT_TRADEMARK_NAME = "Shout - Here & Now\u2122"
 SHOUT_SHORT_NAME = "Shout"
 SHOUT_SHORT_NAME_CAPITAL = "SHOUT"
-SHOUT_POST_NAME = "shout"
+SHOUT_POST_NAME = "message"
 SCORE_NAME = "karma"
 
 CONTACT_EMAIL = "info@street-shout.com"
@@ -19,6 +19,6 @@ SENDER_EMAIL = SENDER_EMAIL_
 FLAG_EMAIL = "info@street-shout.com"
 
 APPLE_STORE_LINK = "http://itunes.apple.com/app/id734887535?mt=8"
-ANDROID_STORE_LINK = ""
+ANDROID_STORE_LINK = "https://play.google.com/store/apps/details?id=com.streetshout.android"
 
 DELETE_ACCOUNT_LINK = "#" #todoBT delete account
