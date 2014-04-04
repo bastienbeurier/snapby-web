fa44419c5b9d1efb06ffa0e92164aea59a4cf0bf
Bastien Beurier <bastienbeurier@gmail.com>
2014-04-03 19:19:28 -0700

    Create welcome activity

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@ class User < ActiveRecord::Base
   has_many :followed_users, through: :relationships, source: :followed
   has_many :followers, through: :reverse_relationships
 
+  after_create :create_welcome_activity
+
   acts_as_mappable  :default_units => :kms, 
                     :default_formula => :sphere, 
                     :distance_field_name => :distance,
@@ -79,5 +81,12 @@ class User < ActiveRecord::Base
 
   def self.response_users(users)
     users.map { |user| user.response_user }
+  end
+
+  def create_welcome_activity
+    self.activities.create!(
+        subject: self, 
+        activity_type: "welcome"
+      )
   end
 end
