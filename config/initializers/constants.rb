04ab8f4741df271a75ac2698eb9ebbb7ac47325d
Bastien Beurier <bastienbeurier@gmail.com>
2014-04-16 11:16:38 -0700

    Change constant values

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -10,31 +10,25 @@ MIN_USERNAME_LENGTH = 1
 
 TRENDING_LIKES_COUNT = 5
 
-SHOUT_COMPANY_NAME = "Street Shout Inc."
-SHOUT_TRADEMARK_NAME = "Shout - Here & Now\u2122"
-SHOUT_SHORT_NAME = "Shout"
-SHOUT_SHORT_NAME_CAPITAL = "SHOUT"
-SHOUT_POST_NAME = "message"
+SHOUT_COMPANY_NAME = "Snapby Inc."
+SHOUT_TRADEMARK_NAME = "Snapby\u2122"
+SHOUT_SHORT_NAME = "Snapby"
+SHOUT_SHORT_NAME_CAPITAL = "SNAPBY"
+SHOUT_POST_NAME = "snapby"
 SCORE_NAME = "karma"
 
 CONTACT_EMAIL = "info@shouthereandnow.com"
 SENDER_EMAIL = SENDER_EMAIL_
 FLAG_EMAIL = "info@shouthereandnow.com"
 
-WEBSITE_HOME = "www.shouthereandnow.com/"
+WEBSITE_HOME = "www.snapbyapp.com/"
 
 APPLE_STORE_LINK = "http://itunes.apple.com/app/id734887535?mt=8"
 ANDROID_STORE_LINK = "https://play.google.com/store/apps/details?id=com.streetshout.android"
-
-FACEBOOK_PAGE_LINK = "https://www.facebook.com/appcenter/612068318842588"
-TWITTER_PAGE_LINK = "https://twitter.com/ShoutHereAndNow"
-
-TOPAPPLI_LINK = "http://www.topappli.fr/nouveaute-decouvrez-ce-quil-se-passe-autour-de-vous-grace-a-shout_12735"
-WEBZEEN_LINK = "http://www.webzeen.fr/high-tech/applications-mobiles/shout-tous-les-evenements-autour-de-vous-en-temps-reel/12419"
 
 DELETE_ACCOUNT_LINK = "#" #todoBT delete account
 
 ANONYMOUS_USERNAME = "Anonymous"
 
-PROFILE_PICTURE_BUCKET_PROD = "shout_profile_pics"
-PROFILE_PICTURE_BUCKET_DEV = "shout_profile_pics_dev"
+PROFILE_PICTURE_BUCKET_PROD = "snapby_profile_pics"
+PROFILE_PICTURE_BUCKET_DEV = "snapby_profile_pics_dev"
